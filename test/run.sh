6b2a4c78552a38d09ce2edab731d2b838cce6a21
Ilya Kolbin <iskolbin@gmail.com>
2016-04-25 15:52:27 +0300

    make test more universal

--- test/run.sh
+++ test/run.sh
@@ -2,6 +2,7 @@
 
 pushd ../
 haxe SpatialHash.hx -js SpatialHash.js
-cat SpatialHash.js | sed '$d' | awk 'NR==1 {print "\"use strict\";"} NR>1 {print;}' > test/SpatialHash.js
+#cat SpatialHash.js | sed '$d' | awk 'NR==1 {print "\"use strict\";"} NR>1 {print;}' > test/SpatialHash.js
+cat SpatialHash.js | sed 's/(function (console) { "use strict";/"use strict";/g' > test/SpatialHash.js
 popd
 http-server
